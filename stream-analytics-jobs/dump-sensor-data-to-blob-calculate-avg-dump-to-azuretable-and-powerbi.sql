5600318dc67707dcc64aef8180f4aff9d8a89514
Radu Vunvulea <vunvulear@outlook.com>
2017-01-04 16:57:05 +0200

    Create job that dump sensor data to blob storage and calculate the avg of sensor for each min. The avg data is dump to Power BI and Azure Table

---
+++ stream-analytics-jobs/dump-sensor-data-to-blob-calculate-avg-dump-to-azuretable-and-powerbi.sql
@@ -0,0 +1,45 @@
+WITH avgdata AS (
+SELECT 
+    deviceId as deviceId,
+    System.TimeStamp as timeslot,
+    AVG(sensorInf.temp) as avgtemp,
+    AVG(sensorInf.humidity) as avghumidity,
+    AVG(sensorInf.distance) as avgdistance,
+    AVG(sensorInf.light) as avglight
+FROM
+    inputDeviceData TIMESTAMP BY EventEnqueuedUtcTime
+WHERE
+    msgType = 'sensorData'
+GROUP BY
+    deviceId,
+    TumblingWindow(second, 60)
+)
+
+SELECT
+    deviceId as deviceId,
+    sensorInf.temp as temp,
+    sensorInf.humidity as humidity,
+    sensorInf.distance as distance,
+    sensorInf.light as light,
+    EventEnqueuedUtcTime as receivedTime
+INTO
+    outputSensorDataBlobStorage
+FROM
+    inputDeviceData TIMESTAMP BY EventEnqueuedUtcTime
+WHERE
+    msgType = 'sensorData'
+
+
+SELECT 
+    *
+INTO 
+    outputSensorDataConsolidatedTableStorage
+FROM
+    avgdata
+
+SELECT 
+    *
+INTO 
+    outputPowerBIDataConsolidated
+FROM
+    avgdata
