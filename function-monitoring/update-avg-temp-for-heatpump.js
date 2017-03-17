a1eec5ec6034217287e53b487028059fe0cf5d06
Radu Vunvulea <vunvulear@outlook.com>
2017-03-17 10:15:07 +0200

    Add an Azure Function that allows us to update the avg temperature.

---
+++ function-monitoring/update-avg-temp-for-heatpump.js
@@ -0,0 +1,25 @@
+process.env["AZURE_STORAGE_ACCOUNT"] = "vunvuleariotstorage";
+process.env["AZURE_STORAGE_ACCESS_KEY"] = "@@@";
+process.env["AZURE_STORAGE_CONNECTION_STRING "] = "@@@";
+
+
+module.exports = function (context, myQueueItem) {
+    var azure = require('azure-storage');
+    var tableSvc = azure.createTableService();
+    tableSvc.createTableIfNotExists('systemstatus', function (error, result, response) {
+        if (!error) {
+            var entityToUpdate = new Object();
+            entityToUpdate.PartitionKey = "system";
+            entityToUpdate.RowKey = "currenttemperature";
+            entityToUpdate.Status = myQueueItem.avgtemp.toString();
+
+            tableSvc.insertOrReplaceEntity('systemstatus', entityToUpdate, function (error, result, response) {
+                if (!error) {
+                    context.log('Avg. temp: ', myQueueItem.avgtemp);
+                }
+            });
+        };
+    });
+
+    context.done();
+};
