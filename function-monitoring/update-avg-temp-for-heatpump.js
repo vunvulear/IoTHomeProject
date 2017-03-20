f55cb8f74113635cdcd1ac7dd60fb1c46c8a60aa
Radu Vunvulea <vunvulear@outlook.com>
2017-03-20 15:13:29 +0200

    Update update-avg-temp-for-heatpump.js

--- function-monitoring/update-avg-temp-for-heatpump.js
+++ function-monitoring/update-avg-temp-for-heatpump.js
@@ -11,7 +11,7 @@ module.exports = function (context, myQueueItem) {
             var entityToUpdate = new Object();
             entityToUpdate.PartitionKey = "system";
             entityToUpdate.RowKey = "currenttemperature";
-            entityToUpdate.Status = myQueueItem.avgtemp.toString();
+            entityToUpdate.Status = myQueueItem.avgtemp;
 
             tableSvc.insertOrReplaceEntity('systemstatus', entityToUpdate, function (error, result, response) {
                 if (!error) {
